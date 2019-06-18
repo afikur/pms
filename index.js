62457d2ccff426a7e85afcc49871f47005a9c6e3
Afikur Rahman <this.afikur@gmail.com>
2019-06-18 07:59:46 +0600

    add swagger api doc

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const patients = require('./routes/patients');
 const doctors = require('./routes/doctors');
 const prescriptions = require('./routes/prescriptions');
 const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger');
 
 const app = express();
 
-mongoose.connect('mongodb://localhost/pms')
+mongoose.connect('mongodb://localhost/pms', { useNewUrlParser: true })
     .then(() => console.log('Connected to MongoDB...'))
     .catch(err => console.error('Could not connect to MongoDB...', err));
 
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use('/api/v1/patients', patients);
 app.use('/api/v1/doctors', doctors);
 app.use('/api/v1/prescriptions', prescriptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
