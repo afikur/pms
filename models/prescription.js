840743d39a2fb1dff6d9d2167d1442d8bf9ac249
Afikur Rahman <this.afikur@gmail.com>
2019-06-18 09:57:23 +0600

    add prescription swagger doc

--- models/prescription.js
+++ models/prescription.js
@@ -1,41 +1,42 @@
-const Joi = require('@hapi/joi');
-const mongoose = require('mongoose');
-const {Schema} = mongoose;
+const Joi = require("@hapi/joi");
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
-const Prescription = mongoose.model('Prescription', new mongoose.Schema({
+const Prescription = mongoose.model(
+  "Prescription",
+  new mongoose.Schema({
     doctor: {
-        type: Schema.Types.ObjectId,
-        ref: 'Doctor',
-        required: true
+      type: Schema.Types.ObjectId,
+      ref: "Doctor",
+      required: true
     },
     patient: {
-        type: Schema.Types.ObjectId,
-        ref: 'Patient',
-        required: true
+      type: Schema.Types.ObjectId,
+      ref: "Patient",
+      required: true
     },
     createdAt: {
-        type: Date,
-        default: Date.now()
+      type: Date,
+      default: Date.now()
     },
     updatedAt: {
-        type: Date,
-        default: Date.now()
+      type: Date,
+      default: Date.now()
     },
-    disease: [{type: String}],
-    medicine: [{type: String}]
-}));
+    disease: [{ type: String }],
+    medicine: [{ type: String }]
+  })
+);
 
 function validatePrescription(prescription) {
-    const schema = {
-        doctor: Joi.string().required(),
-        patient: Joi.string().required(),
-        createdAt: Joi.date(),
-        updatedAt: Joi.date(),
-        disease: Joi.array().items(Joi.string()),
-        medicine: Joi.array().items(Joi.string())
-    };
+  const schema = {
+    createdAt: Joi.date(),
+    updatedAt: Joi.date(),
+    disease: Joi.array().items(Joi.string()),
+    medicine: Joi.array().items(Joi.string())
+  };
 
-    return Joi.validate(prescription, schema);
+  return Joi.validate(prescription, schema);
 }
 
 exports.Prescription = Prescription;
