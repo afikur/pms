55e253bfb9b20ffdd5c2858638e563ef7f37d312
Afikur Rahman <this.afikur@gmail.com>
2019-06-19 05:12:03 +0600

    feat: add update and delete feature in user module

--- routes/users.js
+++ routes/users.js
@@ -68,4 +68,36 @@ router.get('/:id', async (req, res) => {
     }
 });
 
+router.put('/:id', async (req, res) => {
+    try {
+        const {error} = validate(req.body);
+        if (error) return res.status(400).send(error.details[0].message);
+        const {email, password, scope} = req.body;
+
+        const user = await User.findByIdAndUpdate(req.params.id,
+            {email, password, scope}, {new: true});
+
+        if (!user) {
+            return res.status(404).send('The user with the given ID was not found.');
+        }
+
+        res.send(user);
+    }
+    catch (e) {
+        res.status(500).send('Internal server error');
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndRemove(req.params.id);
+        if(!user) {
+            res.status(404).send('The user with the given ID was not found.');;
+        }
+        res.send(user);
+    } catch (e) {
+        res.status(500).send('Internal server error');
+    }
+});
+
 module.exports = router;
