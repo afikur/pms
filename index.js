892eeb111ccae46dea4bf27232c943e912cf7d0c
Afikur Rahman <this.afikur@gmail.com>
2019-06-18 08:19:14 +0600

    change swager.json to swagger.yaml

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-const express  = require('express');
-const mongoose = require('mongoose');
-const patients = require('./routes/patients');
-const doctors = require('./routes/doctors');
-const prescriptions = require('./routes/prescriptions');
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger');
+const express = require("express");
+const mongoose = require("mongoose");
+const patients = require("./routes/patients");
+const doctors = require("./routes/doctors");
+const prescriptions = require("./routes/prescriptions");
+const YAML = require("yamljs");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = YAML.load("./swagger.yaml");
 
 const app = express();
 
-mongoose.connect('mongodb://localhost/pms', { useNewUrlParser: true })
-    .then(() => console.log('Connected to MongoDB...'))
-    .catch(err => console.error('Could not connect to MongoDB...', err));
+mongoose
+  .connect("mongodb://localhost/pms", { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch(err => console.error("Could not connect to MongoDB...", err));
 
 app.use(express.json());
-app.use('/api/v1/patients', patients);
-app.use('/api/v1/doctors', doctors);
-app.use('/api/v1/prescriptions', prescriptions);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api/v1/patients", patients);
+app.use("/api/v1/doctors", doctors);
+app.use("/api/v1/prescriptions", prescriptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
